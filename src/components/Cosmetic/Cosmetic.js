434b7a38089afe927ec926100b6014623809fffe
mohon63 <mohonhossen63@gmail.com>
2021-09-23 17:16:38 +0600

    local host

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { addToDb, deleteFromDb } from '../../utilities/Fackdb';
 
 const cosmetic = (props) => {
-    console.log(props.cosmetic);
-    const { name, gender, price } = props.cosmetic;
+    // console.log(props.cosmetic);
+    const { name, gender, price, _id } = props.cosmetic;
+    const handlePurchase = id => {
+        console.log(id);
+        addToDb(id);
+    }
+    const handleRemove = id => {
+        deleteFromDb(id);
+    }
     return (
         <div>
             <h2>Name: {name}</h2>
-            <p>Price: ${price}</p>
+            <p>ID: {_id} Price: ${price}</p>
             <p>Gender: {gender}</p>
+            <button onClick={() => handlePurchase(_id)}>Purchase</button>
+            <button onClick={() => handleRemove(_id)}>Remove</button>
         </div>
     );
 };
